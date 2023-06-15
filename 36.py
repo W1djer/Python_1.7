e8e034a9ee656b203fad203ede773a9a5f6a5c7a
Evgenii <w1djer@mail.ru>
2023-06-15 23:59:47 +0300

    решена 36 задача

--- 36.py
+++ 36.py
@@ -1,3 +1,6 @@
+# Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца.
+# Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны быть распечатаны.
+
 def print_operation_table(operation, num_rows, num_columns):
     table = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
     for i in table:
